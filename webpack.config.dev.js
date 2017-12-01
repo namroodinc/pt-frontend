870942cb10d590cc4f2b47e835670578d61277b3
namroodinc <a_namrood@hotmail.com>
2017-12-01 22:36:59 +0000

    Added webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+require("dotenv").config();
+
+const extractSass = new ExtractTextPlugin({
+  filename: "../build/styles/[name].css"
+});
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: {
+    app: [
+      "babel-polyfill",
+      "webpack/hot/only-dev-server",
+      "webpack-dev-server/client?http://localhost:5000",
+      path.join(__dirname, "./src/client/entry")
+    ]
+  },
+  output: {
+    path: path.join(__dirname, "/public/build/"),
+    filename: "scripts/[name].js",
+    publicPath: "http://localhost:5000/build/"
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".scss"
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$|\.scss$/,
+        loader: extractSass.extract(
+          {
+            fallback: "style-loader",
+            use: [
+              {
+                loader: "css-loader"
+              },
+              {
+                loader: "postcss-loader"
+              },
+              {
+                loader: "sass-loader",
+                options: {}
+              }
+            ]
+          }
+        )
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.EnvironmentPlugin([
+      "NODE_ENV"
+    ])
+  ]
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+require("dotenv").config();
+
+const extractSass = new ExtractTextPlugin({
+  filename: "../build/styles/[name].css"
+});
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: {
+    app: [
+      "babel-polyfill",
+      path.join(__dirname, "./src/client/entry")
+    ]
+  },
+  output: {
+    path: path.join(__dirname, "/public/build/"),
+    filename: "scripts/[name].js",
+    publicPath: "/build/"
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".scss"
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$|\.scss$/,
+        loader: extractSass.extract(
+          {
+            fallback: "style-loader",
+            use: [
+              {
+                loader: "css-loader"
+              },
+              {
+                loader: "postcss-loader"
+              },
+              {
+                loader: "sass-loader",
+                options: {}
+              }
+            ]
+          }
+        )
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new webpack.EnvironmentPlugin([
+      "NODE_ENV"
+    ])
+  ]
+};
